db0b6f117d2ebabbb0d1cb90a5f721943908b7f1
Yagizhan Avci <ygzhn14@gmail.com>
2019-08-17 15:10:14 +0300

    Updates

---
+++ precache-manifest.daaeff965e906fe90b36b6566557f661.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "765454f0a23e058a1f051a9c9176715c",
+    "url": "/portfolio/static/media/Avatar.765454f0.png"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.53ebc025.js"
+  },
+  {
+    "revision": "1aff80006e272d178da9",
+    "url": "/portfolio/static/js/main.1aff8000.chunk.js"
+  },
+  {
+    "revision": "91b4f07e039d9f265d4d",
+    "url": "/portfolio/static/js/2.91b4f07e.chunk.js"
+  },
+  {
+    "revision": "1aff80006e272d178da9",
+    "url": "/portfolio/static/css/main.94ba11ae.chunk.css"
+  },
+  {
+    "revision": "24598329b673051c9b000bd434ee523f",
+    "url": "/portfolio/index.html"
+  }
+];
