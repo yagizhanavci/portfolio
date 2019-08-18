ff42f799a035bf38095b77db87c58f15583c9e3a
Yagizhan Avci <ygzhn14@gmail.com>
2019-08-18 13:58:12 +0300

    Updates

--- precache-manifest.d12e7b216548dd46577bf9e1d5251ed7.js
+++ precache-manifest.d12e7b216548dd46577bf9e1d5251ed7.js
@@ -8,19 +8,19 @@ self.__precacheManifest = [
     "url": "/portfolio/static/js/runtime~main.53ebc025.js"
   },
   {
-    "revision": "e33cb6001d25fdbaa3fa",
-    "url": "/portfolio/static/js/main.e33cb600.chunk.js"
+    "revision": "4b2a4fb3c14f5fe113c5",
+    "url": "/portfolio/static/js/main.4b2a4fb3.chunk.js"
   },
   {
     "revision": "91b4f07e039d9f265d4d",
     "url": "/portfolio/static/js/2.91b4f07e.chunk.js"
   },
   {
-    "revision": "e33cb6001d25fdbaa3fa",
-    "url": "/portfolio/static/css/main.3c62a386.chunk.css"
+    "revision": "4b2a4fb3c14f5fe113c5",
+    "url": "/portfolio/static/css/main.6c8aadd1.chunk.css"
   },
   {
-    "revision": "672db093d7cff9b95850e0a2cc08e645",
+    "revision": "73f6487e94f4b19bd769531f76502766",
     "url": "/portfolio/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.7740897e70e65349f037553cedda12e7.js"
+  "/portfolio/precache-manifest.d12e7b216548dd46577bf9e1d5251ed7.js"
 );
 
 workbox.clientsClaim();
